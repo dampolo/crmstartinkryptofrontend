0d2790e59372142651dd3c6a067046338bb4106c
Damian Poloczek <damianpoloczek@gmail.com>
2025-10-13 22:34:17 +0200

    dashboard first view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Customers } from './customers/customers';
 import { Apply } from './apply/apply';
 import { Invoices } from './invoices/invoices';
 import { Newcustomer } from './newcustomer/newcustomer';
+import { Dashboard } from './dashboard/dashboard';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'kunden' },
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', component: Dashboard, title: 'Dashboard' },
   { path: 'kunden', component: Customers, title: 'Alle Kunden' },
   { path: 'bewerbungen', component: Apply, title: 'Bewerbungen' },
   { path: 'rechnungen', component: Invoices, title: 'Rechnungen' },
