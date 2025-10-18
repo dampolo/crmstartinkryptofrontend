bd63ad266ec03da05f5ffb793dd414b86b5f6d69
Damian Poloczek <damianpoloczek@gmail.com>
2025-10-18 23:26:49 +0200

    first version of algorithmus

--- src/app/algorithmus/algorithmus.ts
+++ src/app/algorithmus/algorithmus.ts
@@ -1,15 +1,33 @@
+import { DecimalPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-algorithmus',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, DecimalPipe],
   templateUrl: './algorithmus.html',
   styleUrl: './algorithmus.scss'
 })
 export class Algorithmus {
   algorithmusForm: FormGroup;
+  investmentAmount: number = 0;
+  totalAmount: number = 0;
+  totalProvision: number = 0;
+
+  firstStepAmount: number = 0;
+  exchangeSetupAmount: number = 0;
+  buyStrategyAmount: number = 0;
+  walletSetupAmount: number = 0;
+  taxToolAmount: number = 0;
+  ongoingSupportAmount: number = 0;
+
+  firstStepProvision: number = 0.05;
+  exchangeSetupProvision: number = 0.03;
+  buyStrategyProvision: number = 0.05;
+  walletSetupProvision: number = 0.06;
+  taxToolProvision: number = 0.06;
+  ongoingSupportProvision: number = 400;
 
   constructor(private fb: FormBuilder) {
     this.algorithmusForm = this.fb.group(
@@ -54,12 +72,57 @@ export class Algorithmus {
 
 
   onSubmit() {
+    this.totalProvision = 0;
+    this.firstStepAmount = 0;
+    this.exchangeSetupAmount = 0;
+    this.buyStrategyAmount = 0;
+    this.walletSetupAmount = 0;
+    this.taxToolAmount = 0;
+
+
+
     if (this.algorithmusForm.valid) {
-      console.log('Form submitted:', this.algorithmusForm.value);
-      alert('Form erfolgreich gesendet!');
+        const sum = Number(this.algorithmusForm.get("Summe")?.value) || 0;
+        this.investmentAmount = sum
+      
+      if (this.getFirstStepValue()) {
+        this.firstStepAmount = sum  * this.firstStepProvision;
+        this.totalProvision += this.firstStepAmount;
+      }
+
+      if (this.getExchangeSetup()) {
+        this.exchangeSetupAmount = sum * this.exchangeSetupProvision;
+        this.totalProvision += this.exchangeSetupAmount;
+      }
+
+      if (this.getBuyStrategy()){
+        this.buyStrategyAmount = sum * this.buyStrategyProvision;
+        this.totalProvision += this.buyStrategyAmount;
+        
+      }
+
+      if(this.getWalletSetup()) {
+        this.walletSetupAmount = sum * this.walletSetupProvision;
+        this.totalProvision += this.walletSetupAmount;    
+      }
+      
+      if (this.getTaxTool()) {
+        this.taxToolAmount = sum * this.taxToolProvision;
+        this.totalProvision += this.walletSetupAmount;     
+      }
+
+      if (this.ongoingSupport()) {
+
+      }
+      
+      this.totalAmount = sum + this.totalProvision
     } else {
       this.algorithmusForm.markAllAsTouched();
     }
+  }
+
+  setProvisons() {
+
   }
 
   get Summe() {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+registerLocaleData(localeDe);
+
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    { provide: LOCALE_ID, useValue: 'de-DE' } // 🇩🇪 Set locale globally
+  ]
+}).catch(err => console.error(err));
