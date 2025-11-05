c7e73162bcca937e5a7ed5d976a248a74c3fe08e
Damian Poloczek <damianpoloczek@gmail.com>
2025-11-05 08:47:15 +0100

    reset form validation

--- src/app/settings/company/company.ts
+++ src/app/settings/company/company.ts
@@ -21,36 +21,36 @@ export class Company {
       // logo: [null, Validators.required],
       companyName: [this.companyDetails.companyName, [Validators.required, Validators.minLength(2)]],
       street: [this.companyDetails.street, [Validators.required]],
-      number: [this.companyDetails.number, [Validators.required]],
-      postcode: [
-        this.companyDetails.postcode,
-        [
-          Validators.required,
-          Validators.pattern(/^[0-9]{4,5}$/), // German ZIP format (4–5 digits)
-        ],
-      ],
-      city: [this.companyDetails.city, [Validators.required]],
-      ownerName: [this.companyDetails.ownerName, [Validators.required]],
-      taxNumber: [
-        this.companyDetails.taxNumber,
-        [
-          Validators.required,
-          Validators.pattern(/^[0-9]{3}\/[0-9]{4,5}\/[0-9]{4}$/), // e.g. 123/4567/8901
-        ],
-      ],
-      foundingYear: [
-      this.companyDetails.founding,
-      [
-        Validators.required,
-        Validators.pattern(/^[0-9]{4}$/), // 4-digit year only
-        Validators.min(1800),
-        Validators.max(this.currentYear),
-      ],
-    ],
-      bank: [this.companyDetails.bank, Validators.required],
-      bankAccount: [this.companyDetails.bankAccount, [Validators.required]],
-      swiftCode: [this.companyDetails.swiftCode, [Validators.required],
-      ],
+      // number: [this.companyDetails.number, [Validators.required]],
+      // postcode: [
+      //   this.companyDetails.postcode,
+      //   [
+      //     Validators.required,
+      //     Validators.pattern(/^[0-9]{4,5}$/), // German ZIP format (4–5 digits)
+      //   ],
+      // ],
+      // city: [this.companyDetails.city, [Validators.required]],
+      // ownerName: [this.companyDetails.ownerName, [Validators.required]],
+      // taxNumber: [
+      //   this.companyDetails.taxNumber,
+      //   [
+      //     Validators.required,
+      //     Validators.pattern(/^[0-9]{3}\/[0-9]{4,5}\/[0-9]{4}$/), // e.g. 123/4567/8901
+      //   ],
+      // ],
+    //   foundingYear: [
+    //   this.companyDetails.founding,
+    //   [
+    //     Validators.required,
+    //     Validators.pattern(/^[0-9]{4}$/), // 4-digit year only
+    //     Validators.min(1800),
+    //     Validators.max(this.currentYear),
+    //   ],
+    // ],
+      // bank: [this.companyDetails.bank, Validators.required],
+      // bankAccount: [this.companyDetails.bankAccount, [Validators.required]],
+      // swiftCode: [this.companyDetails.swiftCode, [Validators.required],
+      // ],
     });
   }
 
