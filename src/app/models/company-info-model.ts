1e6857f1cd5c6c9d41cc2b5c556624a5d34fd1bf
Damian Poloczek <damianpoloczek@gmail.com>
2025-11-08 21:05:53 +0100

    remove typo in application interface

---
+++ src/app/models/company-info-model.ts
@@ -0,0 +1,15 @@
+export interface CompanyInfo {
+  companyLogo: string;
+  companyName: string;
+  street: string;
+  number: string;
+  postcode: string;
+  city: string;
+  ownerName: string;
+  taxNumber: string;
+  founding: string;
+  email: string;
+  bank: string;
+  bankAccount: string;
+  swiftCode: string;
+}
