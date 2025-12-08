efc2c387ed296b9acc8d607855c021fa8d8c06ef
Damian Poloczek <damianpoloczek@gmail.com>
2025-12-08 23:23:03 +0100

    login checkAuth and logout

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { ConfigService } from '../config.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  isAuthenticated = new BehaviorSubject<boolean>(false);
+
+  constructor(private http: HttpClient, private router: Router, private config: ConfigService) {}
+
+  // Dynamically build endpoint from config.json
+
+  login(username: string, password: string) {
+    return this.http.post(
+      this.config.apiUrl + 'token/',
+      { username, password },
+      { withCredentials: true }
+    );
+  }
+
+  checkAuth() {
+    return this.http.get(this.config.apiUrl + 'me/', { withCredentials: true }).subscribe({
+      next: () => this.isAuthenticated.next(true),
+      error: () => this.isAuthenticated.next(false),
+    });
+  }
+
+    logout() {
+    this.http.post(this.config.apiUrl + 'logout/', {}, { withCredentials: true }).subscribe(() => {
+      this.isAuthenticated.next(false);
+      this.router.navigate(['/login']);
+    });
+  }
+}
