1b7699c7b7791205f9d9352f83b8f04fada29e45
Damian Poloczek <damianpoloczek@gmail.com>
2026-02-23 22:33:28 +0100

    auth responsiv

--- src/app/customer/header-auth/header-auth.ts
+++ src/app/customer/header-auth/header-auth.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Logo } from '../../shared/logo/logo';
 
 @Component({
   selector: 'app-header-auth',
-  imports: [RouterLink],
+  imports: [RouterLink, Logo],
   templateUrl: './header-auth.html',
   styleUrl: './header-auth.scss',
 })
--- src/app/website/main/main.ts
+++ src/app/website/main/main.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AppointmentButton } from '../../shared/appointment-button/appointment-button';
 import { TranslatePipe } from '@ngx-translate/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-main',
-  imports: [AppointmentButton, TranslatePipe],
+  imports: [AppointmentButton, TranslatePipe, RouterLink],
   templateUrl: './main.html',
   styleUrl: './main.scss',
 })
