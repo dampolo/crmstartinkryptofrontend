74a27763c415da524ded1388922a108b53514913
Damian Poloczek <damianpoloczek@gmail.com>
2026-02-25 20:07:42 +0100

    scss responisve

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://127.0.0.1:8000/api/',
-  googleClientId: '1087292982462-4tqctn5ofbs33v8t6dmuf6m0curgb54k.apps.googleusercontent.com'
+  googleClientId: '1087292982462-4tqctn5ofbs33v8t6dmuf6m0curgb54k.apps.googleusercontent.com',
 };
