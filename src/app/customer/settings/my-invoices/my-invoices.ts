e4fd6418189309259c4d1ca02c8567bbedfc15e1
Damian Poloczek <damianpoloczek@gmail.com>
2026-03-11 22:49:12 +0100

    invoice download works on frontend

--- src/app/customer/settings/my-invoices/my-invoices.ts
+++ src/app/customer/settings/my-invoices/my-invoices.ts
@@ -4,33 +4,35 @@ import { INVOICE_CUSTOMER } from '../../../models/invoice.model';
 import { MainStateService } from '../../../main-services/main-state-service';
 import { InvoiceService } from '../../../main-services/invoice-service';
 import { DatePipe } from '@angular/common';
+import { environment } from '../../../../environment/environment';
 
 @Component({
-  selector: 'app-my-invoices',
-  imports: [RouterLink, DatePipe],
-  templateUrl: './my-invoices.html',
-  styleUrl: './my-invoices.scss',
+    selector: 'app-my-invoices',
+    imports: [DatePipe],
+    templateUrl: './my-invoices.html',
+    styleUrl: './my-invoices.scss',
 })
 export class MyInvoices {
-  invoiceService = inject(InvoiceService);
-  mainStateService = inject(MainStateService);
-  openMenuId: number | null = null;
-  invoices = signal<INVOICE_CUSTOMER[]>([]);
+    invoiceService = inject(InvoiceService);
+    mainStateService = inject(MainStateService);
+    openMenuId: number | null = null;
+    invoices = signal<INVOICE_CUSTOMER[]>([]);
+    baseUrl = environment.apiBaseUrl;
 
     ngOnInit(): void {
-    this.invoiceService.getCustomerInvoices().subscribe({
-      next: (invoices) => {
-        this.invoices.set(invoices);
-        console.log(invoices);
-        
-      },
-      error: (err) => {
-        this.mainStateService.displayToast('SystemFehler', false);
-      },
-    });
-  }
+        this.invoiceService.getCustomerInvoices().subscribe({
+            next: (invoices) => {
+                this.invoices.set(invoices);
+                console.log(invoices);
+
+            },
+            error: (err) => {
+                this.mainStateService.displayToast('SystemFehler', false);
+            },
+        });
+    }
 
     toggleMenu(id: number) {
-    this.openMenuId = this.openMenuId === id ? null : id;
-  }
+        this.openMenuId = this.openMenuId === id ? null : id;
+    }
 }
--- src/app/main-services/invoice-service.ts
+++ src/app/main-services/invoice-service.ts
@@ -24,5 +24,4 @@ export class InvoiceService {
       withCredentials: true
     })
   }
-
 }
