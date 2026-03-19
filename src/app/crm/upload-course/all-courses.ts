a0446b8739f08fa0f2b6949440eaf30e3ae7e34f
Damian Poloczek <damianpoloczek@gmail.com>
2026-03-19 21:13:58 +0100

    router first

--- src/app/crm/upload-course/all-courses.ts
+++ src/app/crm/upload-course/all-courses.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { CourseService } from '../../main-services/course-service';
+import { COURSE } from '../../models/course.model';
+import { MainStateService } from '../../main-services/main-state-service';
 
 @Component({
-  selector: 'app-all-courses',
-  imports: [RouterLink],
-  templateUrl: './all-courses.html',
-  styleUrl: './all-courses.scss',
+    selector: 'app-all-courses',
+    imports: [RouterLink, DecimalPipe],
+    templateUrl: './all-courses.html',
+    styleUrl: './all-courses.scss',
 })
 export class AllCourses {
+
+    courseService = inject(CourseService)
+    courses = signal<COURSE[]>([])
+    mainStateService = inject(MainStateService);
+
+    ngOnInit(): void {
+        this.courseService.getCourses().subscribe({
+            next: (courses) => {
+                this.courses.set(courses)
+                console.log(courses);
+
+            },
+            error: (err) => {
+                this.mainStateService.displayToast('SystemFehler', false);
+            }
+        })
+    }
 
 }
