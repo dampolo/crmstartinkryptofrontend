c41a5ceade5d0853d854986356f07fb7cf7f1258
Damian Poloczek <damianpoloczek@gmail.com>
2026-04-01 21:33:39 +0200

    correction price styles in scss

--- src/app/crm/upload-course/all-courses.ts
+++ src/app/crm/upload-course/all-courses.ts
@@ -2,7 +2,7 @@ import { DecimalPipe } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { CourseService } from '../../main-services/course-service';
-import { COURSE } from '../../models/course.model';
+import { COURSE, PURCHASE } from '../../models/course.model';
 import { MainStateService } from '../../main-services/main-state-service';
 
 @Component({
@@ -15,12 +15,24 @@ export class AllCourses {
 
     courseService = inject(CourseService)
     courses = signal<COURSE[]>([])
+    purchasedCourses = signal<PURCHASE[]>([])
+
     mainStateService = inject(MainStateService);
 
     ngOnInit(): void {
         this.courseService.getCourses().subscribe({
             next: (courses) => {
                 this.courses.set(courses)
+
+            },
+            error: (err) => {
+                this.mainStateService.displayToast('SystemFehler', false);
+            }
+        })
+
+        this.courseService.getUserCourses().subscribe({
+            next: (courses) => {
+                this.purchasedCourses.set(courses)
                 console.log(courses);
 
             },
