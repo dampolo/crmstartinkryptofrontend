4426b0d13c1d8857becf8c5c00977366254b97d5
Damian Poloczek <damianpoloczek@gmail.com>
2026-04-07 23:47:56 +0200

    add pdf in add-new-lesson

--- src/app/crm/upload-course/add-new-video/add-new-lesson.ts
+++ src/app/crm/upload-course/add-new-video/add-new-lesson.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from "@angular/router";
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MainStateService } from '../../../main-services/main-state-service';
 import { CourseService } from '../../../main-services/course-service';
 import { Back } from '../../../shared/back/back';
+import { LESSON_PDF } from '../../../models/course.model';
 
 @Component({
     selector: 'app-add-new-lesson',
@@ -20,6 +21,8 @@ export class AddNewLesson {
 
     showEdit = false
 
+    pdfs = signal<LESSON_PDF[]>([])
+
     constructor(private route: ActivatedRoute, private router: Router) {
         this.lessonForm = new FormGroup({
             title: new FormControl("", Validators.required),
@@ -32,7 +35,7 @@ export class AddNewLesson {
 
     sumbitMainData() {
         const courseId = Number(this.route.snapshot.paramMap.get("courseId"))
-        
+
         const payload = {
             course: courseId,
             title: this.lessonForm.value.title,
@@ -69,5 +72,33 @@ export class AddNewLesson {
         this.router.navigate(["/crm/kurse", courseId, "list-of-lessons"])
     }
 
+    deletePdf(pdfId: number) {
+    }
+
+    formatFileSize(size: number): string {
+        if (size < 1024) return size + ' B';
+        if (size < 1024 * 1024) return (size / 1024).toFixed(1) + ' KB';
+        return (size / (1024 * 1024)).toFixed(1) + ' MB';
+    }
+
+    onFileSelected(event: any) {
+        const file = event.target.files[0];
+
+        if (file) {
+            const newPdf: LESSON_PDF = {
+                id: Date.now(),
+                lesson: Date.now(),
+                title: file.name,
+                file: file,
+                file_size_display: this.formatFileSize(file.size),
+                created_at: new Date().toISOString(),
+                updated_at: new Date().toISOString(),
+            };
+
+            this.pdfs.update(pdfs => [...pdfs, newPdf]);
+            console.log(this.pdfs());
+            
+        }
+    }
 
 }
