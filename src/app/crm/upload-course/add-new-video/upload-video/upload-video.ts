aae554535115dfe0227013831c598b656cf61746
Damian Poloczek <damianpoloczek@gmail.com>
2026-04-08 23:31:37 +0200

    new video html css ts and router

--- src/app/crm/upload-course/add-new-video/upload-video/upload-video.ts
+++ src/app/crm/upload-course/add-new-video/upload-video/upload-video.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Back } from '../../../../shared/back/back';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-upload-video',
-  imports: [],
+  imports: [CommonModule, Back],
   templateUrl: './upload-video.html',
   styleUrl: './upload-video.scss',
 })
 export class UploadVideo {
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  backToLessons() {
+    const courseId = Number(this.route.snapshot.paramMap.get("courseId"))
+
+    this.router.navigate(["/crm/kurse", courseId, "add-new-lesson"])
+  }
 
 }
