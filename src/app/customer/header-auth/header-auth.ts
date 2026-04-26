0f20bce29c9c5fb339b921ad216fb342a01acfb3
Damian Poloczek <damianpoloczek@gmail.com>
2026-04-26 21:45:44 +0200

    header auth resposnive

--- src/app/customer/header-auth/header-auth.ts
+++ src/app/customer/header-auth/header-auth.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Logo } from '../../shared/logo/logo';
+import { LogoText } from '../../shared/logo-text/logo-text';
 
 @Component({
   selector: 'app-header-auth',
-  imports: [RouterLink, Logo],
+  imports: [RouterLink, LogoText, Logo],
   templateUrl: './header-auth.html',
   styleUrl: './header-auth.scss',
 })
