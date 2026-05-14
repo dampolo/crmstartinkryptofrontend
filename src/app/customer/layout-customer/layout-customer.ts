44e1b7269206925b52532e7409e13221aa62f980
Damian Poloczek <damianpoloczek@gmail.com>
2026-05-14 21:32:55 +0200

    footer added to layout customer

--- src/app/customer/layout-customer/layout-customer.ts
+++ src/app/customer/layout-customer/layout-customer.ts
@@ -5,10 +5,11 @@ import { HeaderCustomer } from '../header-customer/header-customer';
 import { NavbarCustomer } from '../navbar-customer/navbar-customer';
 import { Toast } from '../../shared/toast/toast';
 import { MainStateService } from '../../main-services/main-state-service';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-layout-customer',
-  imports: [CommonModule, HeaderCustomer, NavbarCustomer, RouterOutlet, Toast],
+  imports: [CommonModule, HeaderCustomer, NavbarCustomer, RouterOutlet, Toast, Footer],
   templateUrl: './layout-customer.html',
   styleUrl: './layout-customer.scss',
 })
