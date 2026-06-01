aeb24c38a25334e1653e255afb874448bf610436
Damian Poloczek <damianpoloczek@gmail.com>
2026-06-01 23:01:38 +0200

    dashboard ready in frontend

--- src/app/models/dashboard.model.ts
+++ src/app/models/dashboard.model.ts
@@ -1,8 +1,11 @@
 import { CUSTOMER_CRM } from "./customer.model";
+import { INVOICE } from "./invoice.model";
 
 export interface DASHBOARD {
   customers_count: number;
   applicants_count: number;
   invioces_count: number;
   latest_customers: CUSTOMER_CRM[];
+  latest_invoices: INVOICE[];
+  latest_applicants: CUSTOMER_CRM[];
 }
