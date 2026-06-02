8549c053a21fdf18656f104f5130dfbf0fb5bfb7
Damian Poloczek <damianpoloczek@gmail.com>
2026-06-02 19:59:31 +0200

    test profile

--- src/app/crm/customers/customer-details/customer-details.spec.ts
+++ src/app/crm/customers/customer-details/customer-details.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomerDetails } from './customer-details';
+import { ActivatedRoute } from '@angular/router';
 
 describe('CustomerDetails', () => {
   let component: CustomerDetails;
@@ -8,7 +9,7 @@ describe('CustomerDetails', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CustomerDetails]
+      imports: [CustomerDetails, ActivatedRoute]
     })
     .compileComponents();
 
--- src/app/shared/profile/profile.spec.ts
+++ src/app/shared/profile/profile.spec.ts
@@ -1,16 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Profile } from './profile';
-
+import { ActivatedRoute, provideRouter } from '@angular/router';
+import { UserService } from '../../customer/services/user-service';
+import { MainStateService } from '../../main-services/main-state-service';
+import { of } from 'rxjs';
 
 describe('CustomerProfile', () => {
   let component: Profile;
   let fixture: ComponentFixture<Profile>;
 
+  const userServiceMock = {
+    getCustomer: jasmine.createSpy().and.returnValue(of(null)),
+    updateCustomer: jasmine.createSpy()
+  };
+
+  const mainStateServiceMock = {
+    displayToast: jasmine.createSpy()
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Profile]
-    })
-    .compileComponents();
+      imports: [Profile],
+      providers: [
+        provideRouter([]),
+
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: () => '1'
+              }
+            }
+          }
+        },
+
+        {
+          provide: UserService,
+          useValue: userServiceMock
+        },
+
+        {
+          provide: MainStateService,
+          useValue: mainStateServiceMock
+        }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Profile);
     component = fixture.componentInstance;
