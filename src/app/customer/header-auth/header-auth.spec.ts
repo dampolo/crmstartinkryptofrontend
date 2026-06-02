cc009a74303a54ab4b405370a6764bec8f1aa5c4
Damian Poloczek <damianpoloczek@gmail.com>
2026-06-02 20:27:42 +0200

    test header

--- src/app/customer/header-auth/header-auth.spec.ts
+++ src/app/customer/header-auth/header-auth.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderAuth } from './header-auth';
+import { provideRouter } from '@angular/router';
 
 describe('HeaderAuth', () => {
   let component: HeaderAuth;
@@ -8,7 +9,9 @@ describe('HeaderAuth', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderAuth]
+      imports: [HeaderAuth],providers: [
+        provideRouter([])
+      ]
     })
     .compileComponents();
 
