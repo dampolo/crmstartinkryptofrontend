3ada03d19dd6f2628018c8fc43e4e7aced2d76d3
Damian Poloczek <damianpoloczek@gmail.com>
2026-06-14 21:50:33 +0200

    test for payment

--- src/app/customer/courses/payment/payment.spec.ts
+++ src/app/customer/courses/payment/payment.spec.ts
@@ -1,12 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Payment } from './payment';
-import { provideRouter } from '@angular/router';
+import { ActivatedRoute, convertToParamMap, provideRouter } from '@angular/router';
 import { COURSE, DISCOUNT_CODE, STATUS, TAX } from '../../../models/course.model';
+import { AuthService } from '../../../main-services/auth-service';
+import { of, throwError } from 'rxjs';
+import { ToastService } from '../../../main-services/toast-service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 describe('Payment', () => {
     let component: Payment;
     let fixture: ComponentFixture<Payment>;
+    let authServiceSpy: jasmine.SpyObj<AuthService>
+    let toastServiceSpy: jasmine.SpyObj<ToastService>;
+
 
     const mockCourse: COURSE = {
         id: 1,
@@ -33,10 +40,38 @@ describe('Payment', () => {
     };
 
     beforeEach(async () => {
+
+        authServiceSpy = jasmine.createSpyObj(
+            'AuthService',
+            ['checkProfileComplete', 'checkAuth']
+        );
+
+        toastServiceSpy = jasmine.createSpyObj('ToastService', [
+            'displayToast'
+        ]);
+
         await TestBed.configureTestingModule({
             imports: [Payment],
             providers: [
-                provideRouter([])
+                provideRouter([]),
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        snapshot: {
+                            paramMap: convertToParamMap({
+                                courseId: '1'
+                            })
+                        }
+                    }
+                },
+                {
+                    provide: AuthService,
+                    useValue: authServiceSpy
+                },
+                {
+                    provide: ToastService,
+                    useValue: toastServiceSpy
+                }
             ]
         })
             .compileComponents();
@@ -154,6 +189,103 @@ describe('Payment', () => {
 
         expect(component.discountedNetPrice()).toBe(0);
         expect(component.grossPriceWithDiscount()).toBe(0);
+    });
+    it('should create checkout payload with course id and discount id', () => {
+        component.course.set(mockCourse);
+        component.discountCode.set(mockDiscountCode);
+
+        expect(component.checkoutPayload()).toEqual({
+            course_id: 1,
+            discount: 1
+        });
+    });
+    it('should create checkout payload with null discount when no discount code', () => {
+        component.course.set(mockCourse);
+        component.discountCode.set(null);
+
+        expect(component.checkoutPayload()).toEqual({
+            course_id: 1,
+            discount: null
+        });
+    });
+    it('should create checkout payload with undefined course_id when no course', () => {
+        component.course.set(null);
+        component.discountCode.set(mockDiscountCode);
+
+        expect(component.checkoutPayload()).toEqual({
+            course_id: undefined,
+            discount: 1
+        });
+    });
+
+    it('should call checkProfileComplete with correct values', () => {
+        component.course.set(mockCourse);
+
+        component.discountCode.set({
+            id: 1,
+            code: 'SAVE10',
+            percent_value: 10
+        });
+
+        component.paymentMethod.set('paypal');
+
+        spyOn(component, 'checkProfileComplete');
+
+        component.submitOrder();
+
+        expect(component.checkProfileComplete).toHaveBeenCalledWith(
+            1, // courseId from route
+            {
+                course_id: 1,
+                discount: 1
+            },
+            'paypal'
+        );
+    });
+
+    it('should call checkPurchase when profile is complete', () => {
+        authServiceSpy.checkProfileComplete.and.returnValue(
+            of({
+                message: 'Profil vollständig'
+            })
+        );
+        spyOn(component, 'checkPurchase');
+
+        const payload = {
+            course_id: 1,
+            discount: null
+        };
+
+        component.checkProfileComplete(1, payload, 'paypal');
+
+        expect(component.checkPurchase).toHaveBeenCalledWith(
+            1,
+            payload,
+            'paypal'
+        );
+    });
+    it('should show backend error message when profile is incomplete', () => {
+        const errorResponse = new HttpErrorResponse({
+            error: {
+                message: 'Ergänze dein Profil',
+                missing_fields: ['first_name']
+            }
+        });
+
+        authServiceSpy.checkProfileComplete.and.returnValue(
+            throwError(() => errorResponse)
+        );
+
+        component.checkProfileComplete(
+            1,
+            { course_id: 1, discount: null },
+            'paypal'
+        );
+
+        expect(toastServiceSpy.displayToast).toHaveBeenCalledWith(
+            'Ergänze dein Profil',
+            false
+        );
     });
 
 });
